6b600943358d375fa6b43640dfa2b0af99ffe900
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-12-16 23:47:50 +0200

    toast

--- src/components/BudgetForm/BudgetForm.jsx
+++ src/components/BudgetForm/BudgetForm.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import Form from '../shared/Form';
 import Label from '../shared/Label';
 import Input from '../shared/Input';
 import Button from '../shared/Button';
+import 'react-toastify/dist/ReactToastify.css';
 
 const labelStyles = `
   margin-bottom: 16px;
@@ -16,6 +18,10 @@ class BudgetForm extends Component {
     onSave: PropTypes.func.isRequired,
   };
 
+  equalZero = () => {
+    toast('Введите число больше 0!');
+  };
+
   handleChange = e => {
     this.setState({
       budget: e.target.value,
@@ -24,6 +30,11 @@ class BudgetForm extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    const { budget } = this.state;
+    if (Number(budget) <= 0) {
+      this.equalZero();
+      return;
+    }
 
     this.props.onSave(Number(this.state.budget));
 
@@ -33,20 +44,25 @@ class BudgetForm extends Component {
   render() {
     const { budget } = this.state;
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <Label customStyles={labelStyles}>
-          Enter your total budget
-          <Input
-            type="number"
-            value={budget}
-            onChange={this.handleChange}
-            placeholder={0}
-            name="budget"
-          />
-        </Label>
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Label customStyles={labelStyles}>
+            Enter your total budget
+            <Input
+              type="number"
+              value={budget}
+              onChange={this.handleChange}
+              placeholder={0}
+              name="budget"
+            />
+          </Label>
 
-        <Button label="Save" type="submit" />
-      </Form>
+          <Button label="Save" type="submit" />
+        </Form>
+        <>
+          <ToastContainer autoClose={3000} />
+        </>
+      </div>
     );
   }
 }
--- src/components/ExpenseForm/ExpenseForm.jsx
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import Form from '../shared/Form';
 import Label from '../shared/Label';
 import Input from '../shared/Input';
 import Button from '../shared/Button';
+import 'react-toastify/dist/ReactToastify.css';
 
 const labelStyles = `
   margin-bottom: 16px;  
@@ -20,6 +22,14 @@ export default class ExpenseForm extends Component {
     onSave: PropTypes.func.isRequired,
   };
 
+  equalZero = () => {
+    toast('Введите число больше 0!');
+  };
+
+  emptyInput = () => {
+    toast('Строка с именем должна быть заполненой!');
+  };
+
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value,
@@ -28,6 +38,17 @@ export default class ExpenseForm extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+
+    const { amount, name } = this.state;
+    if (Number(amount) <= 0) {
+      this.equalZero();
+      return;
+    }
+
+    if (name === '') {
+      this.emptyInput();
+      return;
+    }
 
     this.props.onSave({
       id: shortid.generate(),
@@ -41,29 +62,34 @@ export default class ExpenseForm extends Component {
   render() {
     const { name, amount } = this.state;
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <Label customStyles={labelStyles}>
-          Enter expense name
-          <Input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-          />
-        </Label>
-        <Label customStyles={labelStyles}>
-          Enter expense amount
-          <Input
-            type="number"
-            name="amount"
-            placeholder={0}
-            value={amount}
-            onChange={this.handleChange}
-          />
-        </Label>
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Label customStyles={labelStyles}>
+            Enter expense name
+            <Input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </Label>
+          <Label customStyles={labelStyles}>
+            Enter expense amount
+            <Input
+              type="number"
+              name="amount"
+              placeholder={0}
+              value={amount}
+              onChange={this.handleChange}
+            />
+          </Label>
 
-        <Button label="Add" type="submit" />
-      </Form>
+          <Button label="Add" type="submit" />
+        </Form>
+        <>
+          <ToastContainer autoClose={3000} />
+        </>
+      </div>
     );
   }
 }
