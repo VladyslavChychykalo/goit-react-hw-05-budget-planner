0051dc72aa4025e53800794664d6ca4cb79b1fb3
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-12-16 23:48:54 +0200

    Updates

---
+++ precache-manifest.629315cbae8dcfc4b17db4b7e7a7c320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de19255e3d873cabb9ce9f7cf1a79bc",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "7b72b4209705bbf285f6",
+    "url": "/goit-react-hw-05-budget-planner/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "d2804e25d250bc6f7713",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "7b72b4209705bbf285f6",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.c79928ca.chunk.js"
+  },
+  {
+    "revision": "d49f25c022618fdd97f11bd8e7af8411",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.c79928ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2804e25d250bc6f7713",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.e0384661.chunk.js"
+  },
+  {
+    "revision": "5898b43781fcd94828d3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.8d80df19.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-05-budget-planner/precache-manifest.58bfac5245c0809a13c2760444067ae3.js"
+  "/goit-react-hw-05-budget-planner/precache-manifest.629315cbae8dcfc4b17db4b7e7a7c320.js"
 );
 
 self.addEventListener('message', (event) => {
